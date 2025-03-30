d2ab225b752a3ad603d1edcb92dbd292385b58ea
zinou <zinoutebib24@gmail.com>
2025-03-30 03:44:26 +0100

    implement toggle mode theme

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <main className='container mx-auto p-4'>
+            <h1 className='text-4xl text-bold'>about us</h1>
+            <div>about us description</div>
+        </main>
+
+    )
+}
+
+export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 
 export default function Home() {
-
-  return (<div>
-    <h1 className="text-xl">hello chatgpt app</h1>
-
+  return (<div className="container mx-auto  p-4">
+    <h1 className="text-4xl text-bold">hello chatgpt app</h1>
   </div>
   );
 }
